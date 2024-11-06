6cdcc56f415cd80c5036a431532b288d2af06441
AnakinSkywalker01 <96865423+AnakinSkywalker01@users.noreply.github.com>
2024-11-06 14:19:10 +0530

    Add files via upload

---
+++ json2html.js
@@ -0,0 +1,27 @@
+
+export default function json2html(data) {
+    let html = `
+        <table data-user="ojasben.18@gmail.com">
+            <thead>
+                <tr><th>Name</th><th>Age</th><th>Gender</th></tr>
+            </thead>
+            <tbody>
+    `;
+
+    data.forEach(item => {
+        html += `
+            <tr>
+                <td>${item.Name || ''}</td>
+                <td>${item.Age || ''}</td>
+                <td>${item.Gender || ''}</td>
+            </tr>
+        `;
+    });
+
+    html += `
+            </tbody>
+        </table>
+    `;
+
+    return html;
+}
